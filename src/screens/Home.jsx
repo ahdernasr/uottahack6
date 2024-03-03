98a56a2a9daac3ba23eca72f6208ed422763e011
arohao <119353048+arohao@users.noreply.github.com>
2024-03-02 21:34:11 -0500

    location services

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
-import { StyleSheet, Text, View, Dimensions } from 'react-native'
+import React, { useEffect, 
+  useState
+} from 'react'
+import { StyleSheet, 
+  Text, 
+  View, 
+  Dimensions, 
+  PermissionsAndroid 
+} from 'react-native'
 import {enableLatestRenderer} from 'react-native-maps';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
-
+import * as Location from 'expo-location';
 
 enableLatestRenderer();
 
@@ -22,20 +29,45 @@ const styles = StyleSheet.create({
   },
  });
  
- const Home = ({navigation, route}) => (
+
+
+
+ const Home = ({navigation, route}) => {
+  const [ latitude, setLatitude ] = useState(45.4231)
+  const [ longitude, setLongitude ] = useState(-75.6831)
+  
+  const getUserLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      console.log("Not granted")
+      return;
+    }
+  
+    let location = await Location.getCurrentPositionAsync({});
+    // You can now use location.coords.latitude and location.coords.longitude
+    setLatitude(location.coords.latitude)
+    setLongitude(location.coords.longitude)
+    
+    console.log("Found:", location)
+  };
+  
+  getUserLocation()
+  
+  return (
     <View style={styles.container}>
       <MapView
         provider={PROVIDER_GOOGLE} // remove if not using Google Maps
         style={styles.map}
         region={{
-          latitude: 37.78825,
-          longitude: -122.4324,
+          latitude: latitude,
+          longitude: longitude,
           latitudeDelta: 0.015,
           longitudeDelta: 0.0121,
         }}
       >
       </MapView>
     </View>
- );
+ )};
+
 
 export default Home
